9c713f8891aeea79ee811c05b243b248dbdd9bda
Brokko <brokko.der@gmail.com>
2023-12-27 17:42:44 +0100

    - Added Soul chain item - Added images for all old items

--- src/main/java/com/github/brokko/semihardcore/register/ModItems.java
+++ src/main/java/com/github/brokko/semihardcore/register/ModItems.java
@@ -1,9 +1,9 @@
 package com.github.brokko.semihardcore.register;
 
-import com.github.brokko.semihardcore.BrokkosMod;
+import com.github.brokko.semihardcore.SemiHardcoreMod;
 import com.github.brokko.semihardcore.item.KnifeItem;
 import com.github.brokko.semihardcore.item.ReviveBeaconBlockItem;
-import net.minecraft.world.item.BlockItem;
+import com.github.brokko.semihardcore.item.SoulChainItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,12 +11,17 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BrokkosMod.MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SemiHardcoreMod.MODID);
 
     public static final RegistryObject<Item> SOUL = ITEMS.register("soul",
             () -> new Item(new Item.Properties()
                     .fireResistant()
                     .stacksTo(16)));
+
+    public static final RegistryObject<Item> SOUL_CHAIN = ITEMS.register("soul_chain",
+            () -> new SoulChainItem(new Item.Properties()
+                    .setNoRepair()
+                    .stacksTo(1)));
 
     public static final RegistryObject<Item> KNIFE = ITEMS.register("knife",
             () -> new KnifeItem(new Item.Properties()
@@ -24,11 +29,11 @@ public class ModItems {
                     .stacksTo(1)));
 
     public static final RegistryObject<Item> KNIFE_BLOODY = ITEMS.register("knife_bloody",
-            () -> new KnifeItem(new Item.Properties()
+            () -> new Item(new Item.Properties()
                     .setNoRepair()
                     .stacksTo(1)));
 
-    public static final RegistryObject<Item> REVIVE_BEACON = ITEMS.register("revive_beacon",
+    public static final RegistryObject<Item> REVIVE_BEACON_BLOCKITEM = ITEMS.register("revive_beacon",
             () -> new ReviveBeaconBlockItem(ModBlocks.REVIVE_BEACON.get(), new Item.Properties()
                             .stacksTo(1)
                             .rarity(Rarity.COMMON)));
